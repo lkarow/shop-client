9d552d9aeb8705878a76a12a13f0e1115382da63
Lennart <lkarow@online.de>
2022-08-27 17:28:39 +0200

    add all API calls related to the user
    including the following API calls: login, registration, retrieve data, delete user, update user data

--- src/api/users.js
+++ src/api/users.js
@@ -1,29 +1,86 @@
 import axios from 'axios';
 
 const token = localStorage.getItem('token');
+const username = localStorage.getItem('user');
+
+// Login
+export async function login(username, password) {
+  try {
+    let response = await axios.post(
+      'https://shop-api-2022.herokuapp.com/login',
+      {
+        Username: username,
+        Password: password,
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Register
+export async function register(username, password, email, birthday) {
+  try {
+    await axios.post('https://shop-api-2022.herokuapp.com/users', {
+      Username: username,
+      Email: email,
+      Password: password,
+      Birthday: birthday,
+    });
+  } catch (error) {
+    console.log('Error');
+  }
+}
 
 // Get user data
-export async function getUser(username) {
+export async function getUser() {
   try {
-    let response = axios.get(
+    let response = await axios.get(
       `https://shop-api-2022.herokuapp.com/users/${username}`,
       {
         headers: { Authorization: `Bearer ${token}` },
       }
     );
-    console.log(response.data);
+    return response.data;
   } catch (error) {
     console.log(error);
   }
 }
 
 // Delete account
-export async function deleteUser(username) {
+export async function deleteUser() {
   try {
-    axios.delete(`https://shop-api-2022.herokuapp.com/users/${username}`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-    console.log('Account deleted');
+    await axios.delete(
+      `https://shop-api-2022.herokuapp.com/users/${username}`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    localStorage.clear();
+    window.open('/', '_self');
+    alert('Account deleted');
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Update user data
+export async function updateUser(userData) {
+  try {
+    await axios.put(
+      `https://shop-api-2022.herokuapp.com/users/${username}`,
+      {
+        Username: userData.Username,
+        Password: userData.Password,
+        Email: userData.Email,
+        Birthday: userData.Birthday,
+      },
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    alert('User data changed.');
   } catch (error) {
     console.log(error);
   }
