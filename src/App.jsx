c823c57cb48d1b171d222155a06ca6cad38a0198
Lennart <lkarow@online.de>
2022-08-27 17:35:53 +0200

    refactor & add functionality for updating user data and deleting account

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import NavbarView from './components/NavbarView/NavbarView';
 import CartView from './components/CartView/CartView';
 
 import { getItems } from './api/items';
+import { updateUser } from './api/users';
+import { numberOfItemsInCart } from './utility/utility';
 
 import './App.scss';
 
@@ -14,19 +16,29 @@ export default function App() {
   const [items, setItems] = useState('');
   const [itemsInCart, setItemsInCart] = useState([]);
   const [cartIsOpen, setCartIsOpen] = useState(false);
-  const [user, setUser] = useState();
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     getItems().then((response) => setItems(response));
   }, []);
 
+  // Add item to the cart or, if the item is already in the cart, increase the quantity
   const addToCart = (item) => {
-    setItemsInCart((previous) => [...previous, item]);
+    if (!itemsInCart.some((i) => i.item._id === item._id)) {
+      setItemsInCart((previous) => [...previous, { item: item, amount: 1 }]);
+    }
+    if (itemsInCart.some((i) => i.item._id === item._id)) {
+      const newItemsInCart = itemsInCart.map((obj) => {
+        if (obj.item._id === item._id) return { ...obj, amount: ++obj.amount };
+        return obj;
+      });
+      setItemsInCart(newItemsInCart);
+    }
   };
 
   const removeFromCart = (item) => {
     setItemsInCart((previous) =>
-      previous.filter((savedItem) => savedItem._id !== item._id)
+      previous.filter((savedItem) => savedItem._id !== item.item._id)
     );
   };
 
@@ -38,10 +50,21 @@ export default function App() {
     setUser(loggedUser);
   };
 
+  // Handle edit of user data
+  const handleChangeUserData = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+    console.log(user);
+  };
+
+  // Handle save of edited user data
+  const handleSaveUserData = () => {
+    updateUser(user);
+  };
+
   return (
     <>
       <NavbarView
-        numberOfItemsInCart={itemsInCart.length}
+        numberOfItemsInCart={numberOfItemsInCart(itemsInCart)}
         showCart={showCart}
       />
       <CartView
@@ -57,7 +80,14 @@ export default function App() {
         />
         <Route
           path="/profile"
-          element={<Profile user={user} saveUser={saveUser} />}
+          element={
+            <Profile
+              user={user}
+              saveUser={saveUser}
+              handleChangeUserData={handleChangeUserData}
+              handleSaveUserData={handleSaveUserData}
+            />
+          }
         />
       </Routes>
     </>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,41 +2,200 @@ import React, { useState } from 'react';
 import { Container } from 'react-bootstrap';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 import LoginView from '../components/LoginView/LoginView';
 import RegistrationView from '../components/RegistrationView/RegistrationView';
 
-export default function Profile({ user, saveUser }) {
+import './Profile.scss';
+
+import editIcon from '../assets/img/edit-icon.svg';
+
+import { deleteUser } from '../api/users';
+import { formatDate } from '../utility/utility';
+
+export default function Profile({
+  user,
+  saveUser,
+  handleChangeUserData,
+  handleSaveUserData,
+}) {
   const [isLoginActive, setIsLoginActive] = useState(true);
+  const [displayUsernameEdit, setDisplayUsernameEdit] = useState(false);
+  const [displayPasswordEdit, setDisplayPasswordEdit] = useState(false);
+  const [displayEmailEdit, setDisplayEmailEdit] = useState(false);
+  const [displayBirthdayEdit, setDisplayBirthdayEdit] = useState(false);
 
   const toggleLoginRegistration = () => {
     setIsLoginActive(!isLoginActive);
   };
 
-  return (
-    <Container fluid>
-      <h1>Profile</h1>
+  // Toggle inputs to edit the user data
+  const toggleEditUsername = () => {
+    setDisplayUsernameEdit(!displayUsernameEdit);
+  };
+  const toggleEditPassword = () => {
+    setDisplayPasswordEdit(!displayPasswordEdit);
+  };
+  const toggleEditEmail = () => {
+    setDisplayEmailEdit(!displayEmailEdit);
+  };
+  const toggleEditBirthday = () => {
+    setDisplayBirthdayEdit(!displayBirthdayEdit);
+  };
 
+  return (
+    <Container className="profile-data mt-5 mb-5" fluid>
+      {/* User data and inputs to edit user data */}
       {user && (
         <>
-          <Row>
-            <Col>Username:</Col>
-            <Col>{user.user.Username}</Col>
+          {/* Username */}
+          <Row className="mb-3">
+            <Col id="username" sm={3}>
+              Username:
+            </Col>
+            {!displayUsernameEdit ? (
+              <Col sm={8}>{user.Username}</Col>
+            ) : (
+              <Col sm={8}>
+                <InputGroup className="mb-3">
+                  <Form.Control
+                    type="text"
+                    name="Username"
+                    value={user.Username}
+                    onChange={(e) => handleChangeUserData(e)}
+                    aria-label="Username"
+                    aria-describedby="username"
+                  />
+                </InputGroup>
+              </Col>
+            )}
+            <Col sm={1}>
+              <img
+                src={editIcon}
+                className="edit-icon"
+                alt="Edit"
+                onClick={toggleEditUsername}
+              />
+            </Col>
+            {displayUsernameEdit ? (
+              <Row>
+                <Col sm={{ offset: 3 }}>
+                  <Button
+                    className="m-1"
+                    variant="primary"
+                    onClick={handleSaveUserData}
+                  >
+                    Save
+                  </Button>
+                  <Button
+                    className="m-1"
+                    variant="secondary"
+                    onClick={toggleEditUsername}
+                  >
+                    Cancel
+                  </Button>
+                </Col>
+              </Row>
+            ) : null}
           </Row>
-          <Row>
-            <Col>Email:</Col>
-            <Col>{user.user.Email}</Col>
+          {/* Password */}
+          <Row className="mb-3">
+            <Col id="password" sm={3}>
+              Password:
+            </Col>
+            {!displayPasswordEdit ? (
+              <Col sm={8}>
+                &#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;
+              </Col>
+            ) : (
+              <Col sm={8}>
+                <InputGroup className="mb-3">
+                  <Form.Control
+                    type="password"
+                    name="Password"
+                    value={user.Password}
+                    onChange={(e) => handleChangeUserData(e)}
+                    aria-label="Password"
+                    aria-describedby="password"
+                  />
+                </InputGroup>
+              </Col>
+            )}
+            <Col sm={1}>
+              <img
+                src={editIcon}
+                className="edit-icon"
+                alt="Edit"
+                onClick={toggleEditPassword}
+              />
+            </Col>
           </Row>
-          <Row>
-            <Col>Password:</Col>
-            <Col>&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;</Col>
+          {/* Email */}
+          <Row className="mb-3">
+            <Col id="email" sm={3}>
+              Email:
+            </Col>
+            {!displayEmailEdit ? (
+              <Col sm={8}>{user.Email}</Col>
+            ) : (
+              <Col sm={8}>
+                <InputGroup className="mb-3">
+                  <Form.Control
+                    type="text"
+                    name="Email"
+                    value={user.Email}
+                    onChange={(e) => handleChangeUserData(e)}
+                    aria-label="Email"
+                    aria-describedby="email"
+                  />
+                </InputGroup>
+              </Col>
+            )}
+            <Col sm={1}>
+              <img
+                src={editIcon}
+                className="edit-icon"
+                alt="Edit"
+                onClick={toggleEditEmail}
+              />
+            </Col>
           </Row>
-          <Row>
-            <Col>Birthday:</Col>
-            <Col>{user.user.Birthday}</Col>
+          {/* Birthday */}
+          <Row className="mb-3">
+            <Col id="birthday" sm={3}>
+              Birthday:
+            </Col>
+            {!displayBirthdayEdit ? (
+              <Col sm={8}>{formatDate(user.Birthday)}</Col>
+            ) : (
+              <Col sm={8}>
+                <InputGroup className="mb-3">
+                  <Form.Control
+                    type="text"
+                    name="Birthday"
+                    value={user.Birthday}
+                    onChange={(e) => handleChangeUserData(e)}
+                    aria-label="Birthday"
+                    aria-describedby="birtday"
+                  />
+                </InputGroup>
+              </Col>
+            )}
+            <Col sm={1}>
+              <img
+                src={editIcon}
+                className="edit-icon"
+                alt="Edit"
+                onClick={toggleEditBirthday}
+              />
+            </Col>
           </Row>
         </>
       )}
+      {/* Login and registration form */}
       {!user && isLoginActive ? (
         <LoginView
           toggleLoginRegistration={toggleLoginRegistration}
@@ -46,6 +205,16 @@ export default function Profile({ user, saveUser }) {
       {!user && !isLoginActive ? (
         <RegistrationView toggleLoginRegistration={toggleLoginRegistration} />
       ) : null}
+
+      {user && (
+        <Button
+          onClick={() => deleteUser(user.Username)}
+          className="mt-3"
+          variant="danger"
+        >
+          Delete account
+        </Button>
+      )}
     </Container>
   );
 }
