4bc5e52d1724fced10d83938b6a6875ef19c1c40
Lennart <lkarow@online.de>
2022-09-04 23:48:58 +0200

    add own container for express checkout

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -29,59 +29,57 @@ export default function Checkout({ itemsInCart, removeFromCart }) {
 
   return (
     <Container className="mt-5 mb-5" fluid>
-      <div className="checkout-container">
-        <Row>
-          <Col md={8}>
-            <div className="checkout-steps">
-              {cart && (
-                <>
-                  <CheckoutCart
-                    itemsInCart={itemsInCart}
-                    removeFromCart={removeFromCart}
-                  />
-                  <Link to={`/`}>
-                    <Button className="mt-4" size="lg">
-                      Continue shopping
-                    </Button>
-                  </Link>
-                </>
-              )}
-              {shipping && <CheckoutShipping />}
-              {payment && <CheckoutPayment />}
-            </div>
-          </Col>
-          <Col>
-            <div className="checkout-summary">
-              <CheckoutTotal
-                itemsInCart={itemsInCart}
-                handleClickToCheckout={handleClickToCheckout}
-              />
-              <Button
-                onClick={handleClickToCheckout}
-                className="checkout mt-4"
-                size="lg"
-                variant="warning"
-              >
-                {cart && <span>Proceed to shipping</span>}
-                {shipping && <span>Proceed to payment</span>}
-                {payment && <span>Pay</span>}
+      <Row>
+        <Col md={8}>
+          <div className="checkout-container mb-4">
+            {cart && (
+              <>
+                <CheckoutCart
+                  itemsInCart={itemsInCart}
+                  removeFromCart={removeFromCart}
+                />
+                <Link to={`/`}>
+                  <Button className="mt-4" size="lg">
+                    Continue shopping
+                  </Button>
+                </Link>
+              </>
+            )}
+            {shipping && <CheckoutShipping />}
+            {payment && <CheckoutPayment />}
+          </div>
+        </Col>
+        <Col>
+          <div className="checkout-container mb-4">
+            <CheckoutTotal
+              itemsInCart={itemsInCart}
+              handleClickToCheckout={handleClickToCheckout}
+            />
+            <Button
+              onClick={handleClickToCheckout}
+              className="checkout mt-4"
+              size="lg"
+              variant="warning"
+            >
+              {cart && <span>Proceed to shipping</span>}
+              {shipping && <span>Proceed to payment</span>}
+              {payment && <span>Pay</span>}
 
-                {/* Got to the checkout */}
-              </Button>
-              <hr />
-              <p className="express-checkout">
-                Or proceed with express checkout
-              </p>
-              <ExpressCheckout />
-              <Row>
-                <span className="delivery-information mt-4">
-                  All orders over 50.00 EUR free shipping
-                </span>
-              </Row>
-            </div>
-          </Col>
-        </Row>
-      </div>
+              {/* Got to the checkout */}
+            </Button>
+            <Row>
+              <span className="delivery-information mt-4">
+                All orders over 50.00 EUR free shipping
+              </span>
+            </Row>
+          </div>
+
+          <div className="checkout-container mb-4">
+            <p className="express-checkout">Or proceed with express checkout</p>
+            <ExpressCheckout />
+          </div>
+        </Col>
+      </Row>
     </Container>
   );
 }
